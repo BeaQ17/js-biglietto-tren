8245c9ddab5ece6758cebbb9fdadb6613af7fe2b
BeaQ17 <beaq235@gmail.com>
2021-04-23 22:28:38 +0200

    decimal ceiling

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,3 @@
-/* 
-Istruzioni:
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-- il prezzo del biglietto è definito in base ai km (0.21 € al km)
-- va applicato uno sconto del 20% per i minorenni
-- va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-Questo richiederà un minimo di ricerca.
-*/
-
 //chiedi a utente quanti chilometri vuole percorrere e l'età del passeggero
 var km = parseInt(prompt("Quanti chilometri devi percorrere?"));
 console.log(km);
@@ -30,12 +19,12 @@ msg_3.innerHTML = "Il tuo biglietto costa " + price + " euro";
 
 //va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65
 if (age < 18) {
-    var price_under = price - ((price / 100) * 20);
+    var price_under = Math.round(((price - ((price / 100) * 20))) * 100) / 100.0;
     console.log(price_under);
     document.getElementById("sconto").innerHTML = "Applicato uno sconto del 20% per gli under 18:";
     document.getElementById("prezzo").innerHTML = "Il tuo biglietto costa " + price_under + " euro";
 } else if (age > 65) {
-    var price_over = price - ((price / 100) * 40);
+    var price_over = Math.round(((price - ((price / 100) * 40))) * 100) / 100.0;
     console.log(price_over);
     document.getElementById("sconto").innerHTML = "Applicato uno sconto del 40% per gli over 65:";
     document.getElementById("prezzo").innerHTML = "Il tuo biglietto costa " + price_over + " euro";
